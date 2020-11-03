6ef8fd4f558d1bc5387fdb274905ac48f5b8a754
Your Name <fgomezhobbs@gmail.com>
2020-11-03 16:31:36 -0300

    agregue scrit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log("Fernando")
+
+
+self.addEventListener('push', function(event) {
+    if (!(self.Notification && self.Notification.permission === 'granted')) {
+      return;
+    }
+  
+    var data = {};
+    if (event.data) {
+      data = event.data.json();
+    }
+    var duracion = data.duration || "Something Has Happened";
+    var message = data.answered_by || "Here's something you might want to check out.";
+  
+    console.log(duracion)
+
+})
